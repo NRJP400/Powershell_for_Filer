d6910ef8f1df2945ed40c85b5bf792e65293cb59
Dennis Hagist <d.hagist@web.de>
2011-06-28 11:34:48 +0200

    Upload der Startdatei Initiale Einrichtung

---
+++ run.ps1
@@ -0,0 +1,8 @@
+import-module DataONTAP
+#.\showFiler.ps1 10.88.14.241 root xXx
+#.\showFiler.ps1 filer02 root xXx
+
+#.\showFiler.ps1 172.19.10.50 root xXx
+#.\showFiler.ps1 172.19.10.52 root xXx
+#.\showFiler.ps1 172.19.10.54 root xXx
+.\showFiler.ps1 172.19.10.56 root xXx
